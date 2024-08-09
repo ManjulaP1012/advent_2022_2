2f8cefef96ca4f749ce4009511bc6e74cac5f134
ManjulaP1012 <113912030+ManjulaP1012@users.noreply.github.com>
2024-08-09 15:51:48 +0200

    update enum

--- src/main.rs
+++ src/main.rs
@@ -45,5 +45,25 @@ fn main() -> std::io::Result<()> {
         }
     }
     println!("Total points: {points}");
+
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
+
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
     Ok(())
 }
